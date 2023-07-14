522d02711c76efd33e435d12c4fef0ca916bde3d
maenlong <enlong_ma@bizconf.cn>
2023-07-14 17:26:07 +0800

    [test code]增加测试回调函数

--- demo/qbreakpadtest/main.cpp
+++ demo/qbreakpadtest/main.cpp
@@ -1,6 +1,15 @@
 #include <QApplication>
+#include <QDebug>
+
 #include "QBreakpadHandler.h"
 #include "qbreakpadtestwgt.h"
+
+//onBreakpadCrash实现如下
+void onBreakpadCrash(QString dumpPath)
+{
+    qInfo()<<"捕获到崩溃，现在准备调用发送dump文件到服务端程序~， dump文件路径是:"<<dumpPath;
+    //todo ...
+}
 
 int main (int argc, char *argv[])
 {
@@ -8,8 +17,11 @@ int main (int argc, char *argv[])
 
      QBreakpadInstance.setDumpPath("./crashes");    // 设置生成dump文件路径
 
+     QBreakpadInstance.setCallbackMethod(&onBreakpadCrash);
+
     qBreakpadTestWgt *pWgt = new qBreakpadTestWgt;
     pWgt->show();
 
     return app.exec();
 }
+
